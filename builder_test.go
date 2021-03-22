61498dd57a925d6d8dcff5e2f8c8658db3fa1502
slavabobik <bobik.slava@yandex.ru>
2021-03-22 22:42:31 +0300

    updated test

--- builder_test.go
+++ builder_test.go
@@ -9,11 +9,13 @@ func TestFilterBuilder_Exists(t *testing.T) {
 	filter := Filter()
 	filter.Exists("foo", true)
 	filter.Exists("bar", false)
+	filter.Exists("foobar", true)
 
 	got := filter.selector
 	want := Document{
-		"foo": M{"$exists": true},
-		"bar": M{"$exists": false},
+		"foo":    M{"$exists": true},
+		"bar":    M{"$exists": false},
+		"foobar": M{"$exists": true},
 	}
 
 	if !reflect.DeepEqual(got, want) {
