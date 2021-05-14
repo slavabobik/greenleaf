a3429663d23d7f8f2d93f106841fa4445eece612
slavabobik <bobik.slava@yandex.ru>
2021-05-14 22:00:11 +0300

    added time data type & updated generator logic

--- generator/main.go
+++ generator/main.go
@@ -8,8 +8,30 @@ import (
 	"text/template"
 )
 
-var findSingleOperators = []string{"$eq", "$ne", "$gt", "$gte", "$lt", "$lte"}
-var findSliceOperators = []string{"$in", "$nin"}
+type operatorType int
+
+const (
+	scalar = operatorType(1)
+	slice  = operatorType(2)
+)
+
+type operator struct {
+	name  string
+	value string
+	typ   operatorType
+}
+
+var findOperators = []operator{
+	{"Eq", "$eq", scalar},
+	{"Ne", "$ne", scalar},
+	{"Gt", "$gt", scalar},
+	{"Gte", "$gte", scalar},
+	{"Lt", "$lt", scalar},
+	{"Lte", "$lte", scalar},
+	{"In", "$in", slice},
+	{"Nin", "$nin", slice},
+}
+
 var updateMixedOperators = []string{"$set"}
 
 var builtins = []string{
@@ -49,56 +71,43 @@ func main() {
 }
 
 func generateFilterBuilder() error {
-
-	filterFuncTemplate := `
-// {{.Method}} ...	
-func (f *FilterBuilder) {{.Method}}(field string, value {{.Type}}) *FilterBuilder {
-	return f.addSelector(field, "{{.Operator}}", value)
-}
-`
-
-	tmpl, err := template.New("filter_func").Parse(filterFuncTemplate)
-	if err != nil {
-		return err
-	}
+	tmpl := template.Must(template.ParseFiles("./generator/filter.tmpl"))
 
 	file, err := os.Create("filter.go")
 	if err != nil {
 		return err
 	}
 
-	var buf bytes.Buffer
-	for _, operator := range findSingleOperators {
-		for _, t := range builtins {
+	var filterData []templateData
+	for _, op := range findOperators {
 
-			method := strings.Title(operator[1:]) + strings.Title(t)
-			s := templateData{
-				Method:   method,
-				Type:     t,
-				Operator: operator,
+		// generate methods for builtin types.
+		for _, typ := range builtins {
+			data := templateData{
+				Method:   op.name + strings.Title(typ),
+				Operator: op.value,
 			}
 
-			tmpl.Execute(&buf, s)
-		}
-	}
-
-	for _, operator := range findSliceOperators {
-		for _, t := range builtins {
-			method := strings.Title(operator[1:]) + strings.Title(t)
-			s := templateData{
-				Method:   method,
-				Type:     "[]" + t,
-				Operator: operator,
+			switch op.typ {
+			case scalar:
+				data.Type = typ
+			case slice:
+				data.Type = "[]" + typ
 			}
 
-			tmpl.Execute(&buf, s)
+			filterData = append(filterData, data)
 		}
+
+		// generate methods for time data type.
+		data := templateData{
+			Method:   op.name + "Time",
+			Operator: op.value,
+			Type:     "time.Time",
+		}
+		filterData = append(filterData, data)
 	}
 
-	file.WriteString("// Code generated. DO NOT EDIT.\n")
-	file.WriteString("package greenleaf\n\n")
-	file.WriteString(buf.String())
-
+	tmpl.Execute(file, filterData)
 	return nil
 }
 
@@ -123,17 +132,17 @@ func (u *UpdateBuilder) {{.Method}}(field string, value {{.Type}}) *UpdateBuilde
 	for _, operator := range updateMixedOperators {
 
 		op := strings.Title(operator[1:])
-		for _, t := range builtins {
-			method := op + strings.Title(t)
+		for _, typ := range builtins {
+			method := op + strings.Title(typ)
 			s := templateData{
 				Method:   method,
-				Type:     t,
+				Type:     typ,
 				Operator: operator,
 			}
 
 			tmpl.Execute(&buf, s)
 
-			s.Type = "[]" + t
+			s.Type = "[]" + typ
 			s.Method = method + "Slice"
 
 			tmpl.Execute(&buf, s)
